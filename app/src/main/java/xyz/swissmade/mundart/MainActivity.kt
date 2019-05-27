086a05ccb5b3e2178a49823e034d26b7b0f89b3b
Zaniyar <jaha@zhaw.ch>
2019-05-27 15:57:36 +0200

    Working processor loop, but no face detected

--- app/src/main/java/xyz/swissmade/mundart/MainActivity.kt
+++ app/src/main/java/xyz/swissmade/mundart/MainActivity.kt
@@ -1,37 +1,44 @@
 package xyz.swissmade.mundart
 
+import android.annotation.TargetApi
+import android.app.Activity
+import android.app.PendingIntent.getActivity
+import android.content.Context
+import android.hardware.camera2.CameraAccessException
+import android.hardware.camera2.CameraCharacteristics
+import android.hardware.camera2.CameraManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Environment
 import android.support.annotation.RequiresApi
 import android.support.design.widget.BottomNavigationView
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.util.SparseIntArray
+import android.view.Surface
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
 import android.widget.Toast
-import br.com.netodevel.circle_video_record.BuilderCameraView
-import com.camerakit.CameraKitView
-import com.wonderkiln.camerakit.CameraView
-import kotlinx.android.synthetic.main.activity_main.*
-import com.wonderkiln.camerakit.CameraKitVideo
-import com.wonderkiln.camerakit.CameraKitImage
-import com.wonderkiln.camerakit.CameraKitError
-import com.wonderkiln.camerakit.CameraKitEvent
-import com.wonderkiln.camerakit.CameraKitEventListener
-import java.security.AccessController.getContext
-import android.os.Environment.getExternalStorageDirectory
-import android.view.View
-import java.io.File
-import java.io.FileOutputStream
-import android.R.attr.data
+import com.google.android.gms.tasks.OnFailureListener
+import com.google.firebase.ml.vision.FirebaseVision
+import com.google.firebase.ml.vision.common.FirebaseVisionImage
 import com.google.firebase.ml.vision.common.FirebaseVisionImageMetadata
+import com.google.firebase.ml.vision.face.FirebaseVisionFace
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceContour
 import com.google.firebase.ml.vision.face.FirebaseVisionFaceDetectorOptions
+import com.google.firebase.ml.vision.face.FirebaseVisionFaceLandmark
+import io.fotoapparat.Fotoapparat
+import io.fotoapparat.configuration.CameraConfiguration
+import io.fotoapparat.log.logcat
+import io.fotoapparat.log.loggers
+import io.fotoapparat.parameter.ScaleType
+import io.fotoapparat.selector.*
+import io.fotoapparat.view.CameraView
 
 
 class MainActivity : AppCompatActivity() {
-    private var cameraKitView: CameraKitView? = null
-
+    private var cameraView: CameraView? = null
+    private var fotoapparat: Fotoapparat? = null
     private lateinit var textMessage: TextView
     private val onNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
@@ -51,21 +58,19 @@ class MainActivity : AppCompatActivity() {
         false
     }
 
-    lateinit var cameraView: CameraView
-    var mStatus: Boolean? = false
 
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
     @RequiresApi(Build.VERSION_CODES.JELLY_BEAN)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val navView: BottomNavigationView = findViewById(R.id.nav_view)
-        cameraKitView = findViewById(R.id.camera);
+        cameraView = findViewById(R.id.camera_view)
 
         textMessage = findViewById(R.id.message)
         navView.setOnNavigationItemSelectedListener(onNavigationItemSelectedListener)
-        val isRecording:Boolean = false;
 
-        val button_record:Button = findViewById(R.id.record)
+        val button_record: Button = findViewById(R.id.record)
 
         /*button_record.setOnClickListener {
 
@@ -73,79 +78,181 @@ class MainActivity : AppCompatActivity() {
 
         button_record.setOnClickListener(photoOnClickListener);
 
+        val cameraView: io.fotoapparat.view.CameraView = findViewById(R.id.camera_view)
+        // Real-time contour detection of multiple faces
+        val realTimeOpts = FirebaseVisionFaceDetectorOptions.Builder()
+            .setContourMode(FirebaseVisionFaceDetectorOptions.ALL_CONTOURS)
+            .build()
 
-        /* Round Camera View old Library
-        /**
-         * Build CameraView
-         */
-        cameraView = BuilderCameraView().build(this)
+        val metadata = FirebaseVisionImageMetadata.Builder()
+            .setWidth(480) // 480x360 is typically sufficient for
+            .setHeight(360) // image recognition
+            .setFormat(FirebaseVisionImageMetadata.IMAGE_FORMAT_YV12)
+            .setRotation(this.getRotationCompensation(this,applicationContext))
+            .build()
 
-        /**
-         * Setup Circle Video Record
-         */
-        circle_video_record.setup(findViewById(R.id.teich),cameraView)
-        //circle_video_record.setRecordTime(2000)
-        val button_record:Button = findViewById(R.id.record)
-        button_record.setOnClickListener {
-            mStatus = !mStatus!!;
+        val cameraConfiguration = CameraConfiguration(
+            pictureResolution = highestResolution(), // (optional) we want to have the highest possible photo resolution
+            previewResolution = highestResolution(), // (optional) we want to have the highest possible preview resolution
+            previewFpsRange = highestFps(),          // (optional) we want to have the best frame rate
+            focusMode = firstAvailable(              // (optional) use the first focus mode which is supported by device
+                continuousFocusPicture(),
+                autoFocus(),                       // if continuous focus is not available on device, auto focus will be used
+                fixed()                            // if even auto focus is not available - fixed focus mode will be used
+            ),
+            flashMode = firstAvailable(              // (optional) similar to how it is done for focus mode, this time for flash
+                autoRedEye(),
+                autoFlash(),
+                torch(),
+                off()
+            ),
+            antiBandingMode = firstAvailable(       // (optional) similar to how it is done for focus mode & flash, now for anti banding
+                auto(),
+                hz50(),
+                hz60(),
+                none()
+            ),
+            jpegQuality = manualJpegQuality(90),     // (optional) select a jpeg quality of 90 (out of 0-100) values
+            sensorSensitivity = lowestSensorSensitivity(), // (optional) we want to have the lowest sensor sensitivity (ISO)
+            frameProcessor = { frame -> run {
+                    val image = FirebaseVisionImage.fromByteArray(frame.image, metadata)
+                val detector = FirebaseVision.getInstance()
+                    .getVisionFaceDetector(realTimeOpts)
 
-            if (mStatus == true) {
-                circle_video_record.show()
-            } else {
-                circle_video_record.hide()
-            }
+                val result = detector.detectInImage(image)
+                    .addOnSuccessListener { faces ->
+                        // Task completed successfully
+                        // ...
+                        Log.e("FACE", faces.toString())
+                        for (face in faces) {
+                            val bounds = face.boundingBox
+                            val rotY = face.headEulerAngleY // Head is rotated to the right rotY degrees
+                            val rotZ = face.headEulerAngleZ // Head is tilted sideways rotZ degrees
 
-            /**
-             * Listener callback video file
-             */
-            circle_video_record.setVideoListener {
-                Log.d("video_path", it.absolutePath)
-            }
-        }
-        */
+                            // If landmark detection was enabled (mouth, ears, eyes, cheeks, and
+                            // nose available):
+                            val leftEar = face.getLandmark(FirebaseVisionFaceLandmark.LEFT_EAR)
+                            leftEar?.let {
+                                val leftEarPos = leftEar.position
+                            }
+
+                            // If contour detection was enabled:
+                            val leftEyeContour = face.getContour(FirebaseVisionFaceContour.LEFT_EYE).points
+                            val upperLipBottomContour = face.getContour(FirebaseVisionFaceContour.UPPER_LIP_BOTTOM).points
+
+                            // If classification was enabled:
+                            if (face.smilingProbability != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                                val smileProb = face.smilingProbability
+                            }
+                            if (face.rightEyeOpenProbability != FirebaseVisionFace.UNCOMPUTED_PROBABILITY) {
+                                val rightEyeOpenProb = face.rightEyeOpenProbability
+                            }
+
+                            // If face tracking was enabled:
+                            if (face.trackingId != FirebaseVisionFace.INVALID_ID) {
+                                val id = face.trackingId
+                            }
+                        }
+                    }
+                    .addOnFailureListener(
+                        object : OnFailureListener {
+                            override fun onFailure(e: Exception) {
+                                // Task failed with an exception
+                                // ...
+                            }
+                        })
+
+                }
+            }            // (optional) receives each frame from preview stream
+        )
+
+
+        this.fotoapparat = Fotoapparat(
+            context = this,
+            view = cameraView,                   // view which will draw the camera preview
+            scaleType = ScaleType.CenterCrop,    // (optional) we want the preview to fill the view
+            lensPosition = front(),               // (optional) we want back camera
+            cameraConfiguration = cameraConfiguration, // (optional) define an advanced configuration
+            logger = loggers(                    // (optional) we want to log camera events in 2 places at once
+                logcat()                   // ... in logcat
+            ),
+            cameraErrorCallback = { error -> }   // (optional) log fatal errors
+        )
 
     }
+
     private val photoOnClickListener = object : View.OnClickListener {
         override fun onClick(v: View) {
-            cameraKitView?.captureVideo(CameraKitView.VideoCallback{ cameraKitView, photo: Any? ->
-                val savedPhoto = File(Environment.getExternalStorageDirectory(), "photo.jpg")
-                try {
-                    val outputStream = FileOutputStream(savedPhoto.getPath())
-                    val photo = photo as ByteArray
-                    outputStream.write(photo)
-                    outputStream.close()
-                } catch (e: java.io.IOException) {
-                    e.printStackTrace()
-                    Log.e("CKDemo", "Exception in photo callback")
-                }
-            })
+            val text = "Foto wurde geschossen!"
+            val duration = Toast.LENGTH_SHORT
+
+            val toast = Toast.makeText(applicationContext, text, duration)
+            toast.show()
+            Log.e("FACE", "Miao")
         }
     }
-    fun onVideo(cameraKitView: CameraKitView) {
-        // The File parameter is an MP4 file.
-        Toast.makeText(applicationContext,"Hi",Toast.LENGTH_SHORT).show();
-    }
+
     override fun onStart() {
         super.onStart()
-        cameraKitView!!.onStart()
-    }
-    override fun onResume() {
-        super.onResume()
-        cameraKitView?.onResume()
-    }
-
-    override fun onPause() {
-        cameraKitView?.onPause()
-        super.onPause()
+        fotoapparat?.start()
     }
 
     override fun onStop() {
-        cameraKitView?.onStop()
         super.onStop()
+        fotoapparat?.stop()
     }
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    fun getFrontFacingCameraId(cManager: CameraManager): String? {
+        for (cameraId in cManager.cameraIdList) {
+            val characteristics = cManager.getCameraCharacteristics(cameraId)
+            val cOrientation = characteristics.get(CameraCharacteristics.LENS_FACING)!!
+            if (cOrientation == CameraCharacteristics.LENS_FACING_FRONT) return cameraId
+        }
+        return null
+    }
+    private val ORIENTATIONS = SparseIntArray()
 
-    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<String>, grantResults: IntArray) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        cameraKitView?.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    init {
+        ORIENTATIONS.append(Surface.ROTATION_0, 90)
+        ORIENTATIONS.append(Surface.ROTATION_90, 0)
+        ORIENTATIONS.append(Surface.ROTATION_180, 270)
+        ORIENTATIONS.append(Surface.ROTATION_270, 180)
+    }
+    /**
+     * Get the angle by which an image must be rotated given the device's current
+     * orientation.
+     */
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    @Throws(CameraAccessException::class)
+    private fun getRotationCompensation(activity: Activity, context: Context): Int {
+        // Get the device's current rotation relative to its "native" orientation.
+        // Then, from the ORIENTATIONS table, look up the angle the image must be
+        // rotated to compensate for the device's rotation.
+        val deviceRotation = activity.windowManager.defaultDisplay.rotation
+        var rotationCompensation = ORIENTATIONS.get(deviceRotation)
+
+        // On most devices, the sensor orientation is 90 degrees, but for some
+        // devices it is 270 degrees. For devices with a sensor orientation of
+        // 270, rotate the image an additional 180 ((270 + 270) % 360) degrees.
+        val cameraManager = context.getSystemService(CAMERA_SERVICE) as CameraManager
+
+        val sensorOrientation = cameraManager
+            .getCameraCharacteristics(getFrontFacingCameraId(cameraManager))
+            .get(CameraCharacteristics.SENSOR_ORIENTATION)!!
+        rotationCompensation = (rotationCompensation + sensorOrientation + 270) % 360
+
+        // Return the corresponding FirebaseVisionImageMetadata rotation value.
+        val result: Int
+        when (rotationCompensation) {
+            0 -> result = FirebaseVisionImageMetadata.ROTATION_0
+            90 -> result = FirebaseVisionImageMetadata.ROTATION_90
+            180 -> result = FirebaseVisionImageMetadata.ROTATION_180
+            270 -> result = FirebaseVisionImageMetadata.ROTATION_270
+            else -> {
+                result = FirebaseVisionImageMetadata.ROTATION_0
+                Log.e("ROTATION", "Bad rotation value: $rotationCompensation")
+            }
+        }
+        return result
     }
 }
